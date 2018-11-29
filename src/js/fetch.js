a6c777103116b39120d8af444c631faf2cd411bc
iChrisx10x <christopherx10x@gmail.com>
2018-11-29 14:01:46 -0600

    fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,7 +1,17 @@
-fetch('https://easy-motion.herokuapp.com/blog')
+function fetch() {
+  var misCabeceras = new Headers();
+  var miInit = {
+    method: 'GET',
+    mode: 'cors',
+    headers: { 'Content-Type': 'text' }
+    };
+  fetch('https://easy-motion.herokuapp.com/blog',miInit)
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson);
   });
+}
+
+fetch();
