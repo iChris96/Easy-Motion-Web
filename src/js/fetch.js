22f5475cc80cd5af396817fc1304475d30cf563f
iChrisx10x <christopherx10x@gmail.com>
2018-11-29 13:39:25 -0600

    fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,8 +1,7 @@
-async function getAll(x) {
-    return await fetch('https://easy-motion.herokuapp.com/blog', this.headers).then(function(response) {
-      return response.json();
-    });
-  }
-
-let p = getAll(5);
-console.log(p);
+fetch('https://easy-motion.herokuapp.com/blog')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
